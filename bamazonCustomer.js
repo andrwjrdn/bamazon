9e5d28aaa973e803c46643078917f32da3207100
andrwjrdn <andrewjordanreyes@gmail.com>
2019-05-13 14:21:50 -0700

    npm packaged installed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 //Variables for requires/connection
 
-var sql = require('mysql')
+var mysql = require('mysql')
 var inquirer = require('inquirer')
 var cliTable = require ('cli-table')
 
@@ -18,5 +18,21 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
 	if(err)throw err;
-	console.log("connected as id" + connection.threadId);
+	console.log('-----------------------------------------------');
+	console.log('							WELCOME TO BAMAZON								');
+	console.log('-----------------------------------------------');
+	console.log('');
+	console.log('What are you looking for today?');
+	console.log('');
 });
+
+//FUNCTIONS
+
+//Function for displaying products to user with ID's, names, and prices 	
+
+//Function for asking user the ID of product they would like to buy
+
+//Function to check if there is enough product for user to order, if not error message display
+
+
+
